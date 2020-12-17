a50101264029c4fe44db61f2aa0ccc9ea0263ac8
Mihir Gupta <mihir0699@gmail.com>
2020-12-17 18:46:28 +0530

    Some featured added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useEffect, useState, useRef} from 'react'
 import Img from './images/mihir0699.jpg'
 import Chattel from './images/Chattel.jpg'
+import Menu from './images/menu.png'
 import {motion, AnimatePresence} from 'framer-motion';
 import { Card , Input, Button} from 'antd';
 import { MailTwoTone, HomeTwoTone} from '@ant-design/icons';
@@ -127,7 +128,7 @@ const handleSubmit = async(e)=>{
           <NavLink to='/' onClick={()=>{education_ref.current.scrollIntoView({behavior:'smooth', block:'center'})}}>
             Education
           </NavLink>
-          <NavLink to='/' onClick={()=>{contact_ref.current.scrollIntoView({behavior:'smooth', block:'center'})}} style={{paddingRight:'2rem'}}>
+          <NavLink to='/' onClick={()=>{contact_ref.current.scrollIntoView({behavior:'smooth', block:'center'})}} style={{marginRight:'2rem'}}>
             Contact Me
           </NavLink>
           {/* Second Nav */}
@@ -138,9 +139,8 @@ const handleSubmit = async(e)=>{
       </div>
     <IconContext.Provider value={{ color: '#fff' }}>
         <div className='navbar'>
-          <Link to='#' className='menu-bars'>
-          <img src="https://img.icons8.com/metro/26/000000/menu.png" onClick={showSidebar} className="ham"/>
-           
+          <Link to='#' className='menu-bars' onClick={showSidebar}>
+          <img src={Menu} className="ham" style={{height:'15px'}}/>
           </Link>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'} style={{zIndex:'1000'}}>
@@ -199,16 +199,16 @@ const handleSubmit = async(e)=>{
         <div className="image"><img src={Img} className="my_image"/></div>
         <div className="intro_info">
         <span className="intro_head">Hi, myself Mihir</span><br/>
-        <br/> I am a passionate and self taught MERN Stack developer and a competitive programmer. I am currently a third year computer science student pursuing Bachelor of Technology at JSS Academy of Technical Education, Noida. 
-         In my free time you will find me playing Among us 🤪 or scrolling <a href="https://twitter.com/mihir0699" target="_blank" className="twitter"> Twitter </a> 🤳. 
+        <br/> I am a passionate self taught MERN Stack developer and a competitive programmer. I am currently a third year computer science student pursuing Bachelor of Technology at JSS Academy of Technical Education, Noida. 
+         In my free time you will find me playing "Among Us" 🤪 or scrolling <a href="https://twitter.com/mihir0699" target="_blank" className="twitter"> Twitter </a> 🤳. 
          <br/>
-        <a href="https://drive.google.com/file/d/1IOu6mtGUaGAChfOjg3IXgw1LiaI6PQoA/view?usp=sharing" target="_blank" > <button class="resume">Resume</button></a>
+        <a href="https://drive.google.com/file/d/1IOu6mtGUaGAChfOjg3IXgw1LiaI6PQoA/view?usp=sharing" target="_blank" > <button class="resume">Resume  &nbsp;<i class="far fa-file"></i></button></a>
         </div>
     
         <div>
-        <a href="https://github.com/mihir0699" target="_blank">  <img src="https://img.icons8.com/fluent/48/000000/github.png"/></a>
-      <a href="https://www.instagram.com/mihir_gupta_1" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
-      <a href="https://www.linkedin.com/in/mihir0699/" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png"/></a>
+       <a href="https://github.com/mihir0699" target="_blank">  <img  className="social_icons" src="https://www.flaticon.com/svg/static/icons/svg/2111/2111425.svg"/></a>
+      <a href="https://www.instagram.com/mihir_gupta_1" target="_blank"><img  className="social_icons" src="https://www.flaticon.com/svg/static/icons/svg/2111/2111463.svg"/></a>
+      <a href="https://www.linkedin.com/in/mihir0699/" target="_blank"><img className="social_icons" src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"/></a>
         </div>
        
       </motion.div>
@@ -223,27 +223,63 @@ const handleSubmit = async(e)=>{
           Skills
         </h1>
         <div className="skills_icons">
+          
        <div className="skills_icon">
+         <div className="flip-card-inner">
+      <div className="flip-card-front">
        <img src="https://img.icons8.com/color/96/000000/c-plus-plus-logo.png"/>
        </div>
+       <div className="flip-card-back">C++</div>
+       </div>
+       </div>
+
        <div className="skills_icon">
-       <img src="https://img.icons8.com/color/96/000000/javascript.png"/>
+       <div className="flip-card-inner">
+      <div className="flip-card-front">
+       <img src="https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-512.png"/>
+        </div>
+        <div className="flip-card-back">JavaScript</div>
+        </div>
+       </div>
+
+       <div className="skills_icon">
+       <div className="flip-card-inner">
+      <div className="flip-card-front">
+       <img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/233_Node_Js_logo-512.png"/>
+       </div>
+       <div className="flip-card-back">Node.js</div>
+       </div>
        </div>
        <div className="skills_icon">
-       <img src="https://img.icons8.com/color/96/000000/nodejs.png"/>
+       <div className="flip-card-inner">
+      <div className="flip-card-front">
+       <img src="https://img.icons8.com/color/96/000000/mongodb.png" />
+       </div>
+       <div className="flip-card-back">MongoDB</div>
+       </div>
        </div>
        <div className="skills_icon">
-       <img src="https://img.icons8.com/color/96/000000/mongodb.png"/>
+       <div className="flip-card-inner">
+      <div className="flip-card-front">
+       <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"/>
+       </div>
+       <div className="flip-card-back">React.js</div>
+       </div>
        </div>
        <div className="skills_icon">
-       <img src="https://img.icons8.com/officel/96/000000/react.png"/>
+       <div className="flip-card-inner">
+      <div className="flip-card-front">
+       <img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png"/>
+       </div>
+       <div className="flip-card-back">Python</div>
+       </div>
        </div>
        <div className="skills_icon">
-       <img src="https://img.icons8.com/color/96/000000/python.png"/>
+  
+       <img src="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png"/>
+  
        </div>
-       <div className="skills_icon">
-       <img src="https://img.icons8.com/color/96/000000/firebase.png"/>
-       </div>
+      
        </div>
       </motion.div>
       </div>
